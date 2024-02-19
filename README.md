6fc5bce04e080374fda960759036e6c05a1d7d87
Arthur <arthur.decaen@epitech.eu>
2024-02-19 18:58:54 +0100

    FEAT: better Project FIles

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Scripts for a better experience with Epitech MSC intranet
 
+## Installation
+
+1. Install the [Tampermonkey](https://www.tampermonkey.net/) extension for your browser.
+
+2. Select the desired feature's title and then click on 'install'.
+
 ## Features
 
 - [Better Competencies interface](https://greasyfork.org/en/scripts/487732-better-competencies-gandalf)
@@ -10,8 +16,15 @@ Scripts for a better experience with Epitech MSC intranet
   - Highlights failed skills in categories still being validated.
   - Changes the colour of the progress bars according to completion.
 
-## Installation
+<p align="center">
+<img src="pictures/betterCompetencies.png" height="300px">
+<img src="pictures/betterCompetencies2.png" height="300px">
+<img src="pictures/betterCompetencies3.png" height="300px">
+</p>
 
-1. Install the [Tampermonkey](https://www.tampermonkey.net/) extension for your browser.
+- [Better Project Files]()
+  - Adds a "Files" section to each project, allowing access to all files without having to go to each page
 
-2. Select the desired feature's title and then click on 'install'.
+<p align="center">
+<img src="pictures/betterProjectFile.png" width="500px">
+</p>
---
+++ src/betterProjectFiles.js
@@ -0,0 +1,135 @@
+// ==UserScript==
+// @name         Fetch all project files
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-19
+// @description  Rassemble tous les fichiers d'un projet sur la page de ce dernier. Plus besoin d'aller voir chaque lien.
+// @author       You
+// @match        https://gandalf.epitech.eu/course/view.php*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+class File {
+    constructor(title, url, date, icon) {
+        this.title = title
+        this.url = url
+        this.date = date
+        this.icon = icon
+    }
+}
+
+async function fetchSite(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(`Erreur de requête: ${response.status}`);
+        return await response.text();
+    } catch (error) {
+        console.error(`Erreur: ${error.message}`);
+    }
+}
+
+function extractFilesFromPageContent(pageContent) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(pageContent, 'text/html');
+    const filesdiv = doc.querySelectorAll('.fp-filename-icon, .fileuploadsubmission');
+    const filesData = []
+
+    filesdiv.forEach(file => {
+        try {
+            const name = file.textContent
+            const url = file.querySelectorAll('a[href]')[0]?.href
+            const date = file?.nextSibling?.innerText
+            const icon = file.querySelectorAll('.icon.icon')[0]?.src
+            if (!url) return
+            filesData.push(new File(name, url, date ?? "", icon))
+        } catch(e) { console.log(e) }
+    });
+
+    return filesData
+}
+
+function preparePage() {
+    const accordion = document.querySelectorAll('.accordion.collapsibletopics')[0]
+    const newDiv = document.createElement('div');
+    newDiv.textContent = 'Contenu du nouveau div';
+
+    newDiv.innerHTML = `<div class="containerLinks">
+    <span class="sectionname linkName">All files</span>
+    </div>`
+
+    accordion.insertBefore(newDiv, accordion.firstChild.nextSibling);
+
+    return newDiv
+}
+
+function addToPage(files, zone) {
+    const filesDiv = files.map((file) => {
+        return `<div class="links">
+        <img src="${file.icon}"></img>
+        <a href="${file.url}" title="${file.title}">${file.title}</a>
+        <span>${file.date}</span>
+        </div>`
+    }).join("")
+    zone.innerHTML += `${filesDiv}`
+}
+
+(function() {
+    'use strict';
+
+    const styles = `
+    .containerLinks {
+        background: #f4f4f0;
+        margin-bottom: 10px;
+        font-size: 1rem;
+        padding: 10px;
+        padding-left: 25px;
+        padding-right: 25px;
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+    }
+
+    .links {
+        display: grid;
+        grid-template-columns: 20px 1fr 1fr;
+        grid-template-rows: 1fr;
+        grid-column-gap: 8px;
+        text-wrap: nowrap;
+    }
+
+    .links img {
+        width: 20px;
+    }
+
+    .links a {
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+    .sectionname.linkName {
+        margin-bottom: 5px;
+        font-size: 1.5rem;
+        width: fit-content;
+    }
+    `
+
+    var styleSheet = document.createElement("style")
+    styleSheet.innerText = styles
+    document.head.appendChild(styleSheet)
+
+    const linksDiv = document.querySelectorAll(".activityinstance")
+    const links = []
+    for (var i = 0; i != linksDiv.length; i++) {
+        const a = linksDiv[i].getElementsByTagName("a")[0]
+        links.push(a.href)
+    }
+
+    preparePage()
+    const zone = document.querySelectorAll('.containerLinks')[0]
+
+    links.forEach(async (link) => {
+        const pageContent = await fetchSite(link)
+        const files = extractFilesFromPageContent(pageContent)
+        addToPage(files, zone)
+    })
+})();
